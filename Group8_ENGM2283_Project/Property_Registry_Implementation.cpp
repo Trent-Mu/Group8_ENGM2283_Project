c74de15346689c37e29d300ada23f021bbf644e9
Masa Alkaraki <ms485220@dal.ca>
2026-03-15 00:48:50 -0300

    child class method implementation base class method implementation fixed "residential" typo (it was residental before)

--- Group8_ENGM2283_Project/Property_Registry_Implementation.cpp
+++ Group8_ENGM2283_Project/Property_Registry_Implementation.cpp
@@ -8,7 +8,7 @@ owner::owner(string n, int id, string em) : name(n), tax_id(id), email(em) {}
 
 property::property(string n, int id, string em, int bought, int built, string adr, float sqft, float mpr) : guy(n, id, em), info(bought, built), address(adr), square_feet(sqft), market_price(mpr) {}
 
-residental::residental(string n, int id, string em, int bought, int built, string adr, float sqft, float mpr, int bdc): property(n, id, em, bought, built, adr, sqft, mpr), bedroom_count(bdc) {}
+residential::residential(string n, int id, string em, int bought, int built, string adr, float sqft, float mpr, int bdc): property(n, id, em, bought, built, adr, sqft, mpr), bedroom_count(bdc) {}
 
 commercial::commercial(string n, int id, string em, int bought, int built, string adr, float sqft, float mpr, string btype) : property(n, id, em, bought, built, adr, sqft, mpr), business_type(btype) {}
 //_____________________________________________________________________________________________________________________________________________________________________________________________
@@ -16,6 +16,8 @@ commercial::commercial(string n, int id, string em, int bought, int built, strin
 
 // METHOD IMPLEMENTATION
 
+
+// BASE CLASS METHOD IMPLEMENTATION
 float property::get_square_feet() const {
 	return square_feet;
 }
@@ -23,5 +25,47 @@ float property::get_square_feet() const {
 string property::get_address() const {
 	return address;
 }
+
+void property::create_property(string n, int id, string em, int bought, int built, string adr, float sqft, float mpr){
+	guy = owner(n, id, em);
+	info = date(bought, built);
+	address = adr;
+	square_feet = sqft;
+	market_price = mpr;
+}
+
+void property::display() const {
+	guy.display();
+	info.display();
+	cout << "Address: " << address << endl;
+	cout << "Square Footage : " << square_feet << " sq ft" << endl;
+	cout << "Market Price: $" << market_price << endl;
+}
+
+// CHILD CLASS METHOD IMPLEMENTATION
+
+void residential::create_property(string n, int id, string em, int bought, int built, string adr, float sqft, float mpr, int bdc) {
+	property::create_property(n, id, em, bought, built, adr, sqft, mpr);
+	bedroom_count = bdc;
+}
+
+void residential::display() const {
+	cout << "Residential Property: " << endl;
+	property::display();
+	cout << "Bedrooms: " << bedroom_count << endl;
+}
+
+void commercial::create_property(string n, int id, string em, int bought, int built, string adr, float sqft, float mpr, string btype) {
+	property::create_property(n, id, em, bought, built, adr, sqft, mpr);
+	business_type = btype;
+}
+
+void commercial::display() const {
+	cout << "Commercial Property: " << endl;
+	property::display();
+	cout << "Business Type: " << business_type << endl;
+}
+
+
 //_____________________________________________________________________________________________________________________________________________________________________________________________
 
