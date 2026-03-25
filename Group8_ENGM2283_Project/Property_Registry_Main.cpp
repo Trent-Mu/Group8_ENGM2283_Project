f942c0d25b6c52f722ba859c808d6d73570f43cd
Masa Alkaraki <ms485220@dal.ca>
2026-03-25 14:59:27 -0300

    delete function

--- Group8_ENGM2283_Project/Property_Registry_Main.cpp
+++ Group8_ENGM2283_Project/Property_Registry_Main.cpp
@@ -123,29 +123,46 @@ int main(void) {
 			cin >> choice;
 			choice = (char)tolower(choice);
 			if (choice == 'p') {
-				sort(list, choice);
+				Sort(list, choice);
 			}
 			else if (choice == 's') {
-
+				Sort(list, choice);
 			}
 			else if (choice == 'x') {
 				break;
-			}
-			else {
-				cout << "Invalid input. Enter either P, S, or X.";
 			}
 			
 		}
 		//for this i want someone to prompt user for what they are deleting, honetly just do the same for search but replace stuff with delete...
 		else if (choice == 'd') {
 			while (true) {
-				
-				
-				
-				if (choice == 'x') break;
 
+			cout << "Delete: address (A), owner name (N) or email (E)";
+			cin >> choice;
 
-
+			choice = (char)tolower(choice);
+			string del;
+			if (choice == 'a') {
+				cout << "Which address would you like to delete?";
+				cin >> del;
+				remove(list, del, choice);
+			}
+			else if (choice == 'n') {
+				cout << "Which owner name would you like to delete?";
+				cin >> del;
+				remove(list, del, choice);
+			}
+			else if (choice == 'i') {
+				cout << "Which owner ID would you like to delete?";
+				cin >> del;
+				remove(list, del, choice);
+			}
+			else if (choice == 'e') {
+				cout << "Which email would you like to delete?";
+				cin >> del;
+				remove(list, del, choice);
+			}
+			else if (choice == 'x') break;
 
 			}
 		}
